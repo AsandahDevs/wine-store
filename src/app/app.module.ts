af61887c2993b14b12c1aab817e92640e5dc0f87
Asanda <noelmajola@gmail.com>
2023-01-01 23:37:39 +0200

    Remove NavComponent import. Import homepage,products,shoppingcart and register components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, HomepageComponent, ProductsComponent, ShoppingcartComponent, RegisterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
