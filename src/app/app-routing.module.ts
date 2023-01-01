f3095032cbcf2a801c3b312b6beeb7aa389813c2
Asanda <noelmajola@gmail.com>
2023-01-01 23:33:22 +0200

    Add route paths to homepage,products,shopping cart and register components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'shoppingcart', component: ShoppingcartComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
