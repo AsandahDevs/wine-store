5a022536730e4688fc1f93c5ec16a2f22efc0e29
Asanda <noelmajola@gmail.com>
2023-01-02 18:33:46 +0200

    Import formsModule and ReactiveFormsModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -9,8 +9,14 @@ import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, ProductsComponent, ShoppingcartComponent, RegisterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    ProductsComponent,
+    ShoppingcartComponent,
+    RegisterComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
