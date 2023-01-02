23b38e58e664195c6e7704aedfd007a9dfe2609e
Asanda <noelmajola@gmail.com>
2023-01-02 18:45:28 +0200

    Import FormControl,FormGroup and Validators from angular-forms library and created a new instance of registration form.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-
+  registrationForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    emailAddress: new FormControl('', [Validators.required, Validators.email]),
+    physicalAddress: new FormControl('', [Validators.required]),
+    suburb: new FormControl(''),
+    province: new FormControl(''),
+    sex: new FormControl('', [Validators.required]),
+    checkMark: new FormControl(''),
+  });
 }
+
+// todo: create a function that will create an alert to show the user of their submitted form.
+// todo: initially the function shouldn't submit any data to a backend.
