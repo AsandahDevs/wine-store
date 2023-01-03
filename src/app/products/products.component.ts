5c6788832e65adc85967ae9e1cd6931d053c860a
Asanda <noelmajola@gmail.com>
2023-01-03 22:36:46 +0200

    Import productsServices and implemented logic for retrieving products using productsServices.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecommendedWine } from '../interface/recommended-wines';
+import { Root } from '../interface/root';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  wineList: RecommendedWine[] | undefined;
+  constructor(private productsService: ProductsService) {}
 
+  ngOnInit(): void {
+    this.productsService.getListOfRecommendedMerlots().subscribe({
+      next: (data: Root) => {
+        const { recommendedWines } = data;
+        this.wineList = recommendedWines;
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+    });
+  }
 }
