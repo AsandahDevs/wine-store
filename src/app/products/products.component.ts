9f67ffc5c80eb77095c07ebb4f580ab7ee80b7fd
Asanda <noelmajola@gmail.com>
2023-01-05 15:08:48 +0200

    Import subscription resource from rxjs and cart service from services. Add function to display cart items.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { RecommendedWine } from '../interface/recommended-wines';
 import { Root } from '../interface/root';
+import { CartService } from '../services/cart.service';
 import { ProductsService } from '../services/products.service';
 
 @Component({
@@ -9,18 +11,33 @@ import { ProductsService } from '../services/products.service';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-  wineList: RecommendedWine[] | undefined;
-  constructor(private productsService: ProductsService) {}
+  wineList!: RecommendedWine[];
+  merlots$!: Subscription;
+
+  constructor(
+    private productsService: ProductsService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
-    this.productsService.getListOfRecommendedMerlots().subscribe({
-      next: (data: Root) => {
-        const { recommendedWines } = data;
-        this.wineList = recommendedWines;
-      },
-      error: (error: any) => {
-        console.log(error);
-      },
-    });
+    this.merlots$ = this.productsService
+      .getListOfRecommendedMerlots()
+      .subscribe({
+        next: (data: Root) => {
+          const { recommendedWines } = data;
+          this.wineList = recommendedWines;
+        },
+        error: (error: any) => {
+          console.log(error);
+        },
+      });
+  }
+
+  displayCartItems() {
+    return this.cartService.cart;
+  }
+
+  ngOnDestroy() {
+    this.merlots$.unsubscribe();
   }
 }
