900300bd2560b49c67ade309666b62a40217054c
Asanda <noelmajola@gmail.com>
2023-01-05 15:22:09 +0200

    Add service to display cart items.

--- src/app/shoppingcart/shoppingcart.component.ts
+++ src/app/shoppingcart/shoppingcart.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { CartService } from '../services/cart.service';
+
 @Component({
   selector: 'app-shoppingcart',
   templateUrl: './shoppingcart.component.html',
-  styleUrls: ['./shoppingcart.component.scss']
+  styleUrls: ['./shoppingcart.component.scss'],
 })
 export class ShoppingcartComponent {
+  constructor(private cartService: CartService) {}
 
+  displayCartItems() {
+    return this.cartService.getItems();
+  }
 }
